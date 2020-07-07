bc7a2006ec51e0c8e300a25eaed96a972962d769
Akijunior <suitsu19@gmail.com>
2020-07-07 16:26:16 -0300

    Ajustes na data da notificação (testes).

--- src/pages/ScheduleNotification.js
+++ src/pages/ScheduleNotification.js
@@ -8,11 +8,16 @@ const ScheduleNotification = () => {
 
     const handleClick = () => {
 
+        console.log("Atual ", Date.now());
+        const date2 = new Date("2020-07-07T16:25:00");
+        console.log("Improvisada ", date2);
+
         Notification
             .configure()
             .localNotificationSchedule({ // notificação agendada
                 message: 'Nova Notificação',
-                date: new Date(Date.now() + 10000)
+                // date: new Date(Date.now() + 120 * 1000)
+                date: new Date("2020-07-07T16:25:00")
             });
     };
 
@@ -20,7 +25,6 @@ const ScheduleNotification = () => {
         <View style={{ flex: 1, justifyContent: 'center', padding: 20 }}>
             <Button onPress={handleClick}
                 title="Notificar"
-
             />
         </View>
     );
